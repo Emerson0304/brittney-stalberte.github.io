45ffa8f8bfc6177eaf75973971752f4fd3e6dcdd
Brittney Stalberte <emersonb3445@gmail.com>
2023-08-21 17:57:08 +0000

    describe commit

---
+++ Studies/variables.js
@@ -0,0 +1,36 @@
+/**
+ * VARIABLES
+ * 
+ * 0: Variables are named containers for data in JavaScript. We create variables using declarative keywords...
+ * 
+ * 
+ * 1: Declaration and assigment: To create a variable in JavaScript we must first DECLARE it using on of three declartive keywords.
+ * 
+ * 
+ * 2: var, let, const: var, let, and const are the three declarative keywords.
+ * 
+ * 
+ * 
+ * 3: Hoisting:(describe hoisting)
+ * 
+ */
+
+//1. Declaration and assignments //
+//Declaration
+//At the declaration phase...
+var age;
+
+// Assignment
+// age is now assigned the number 30 as it's value.
+age = 30;
+
+
+// 2. var, let and const //
+
+// var //
+
+
+// let //
+
+
+// const //
---
+++ projects/scratch-pad/day-3/first-class-functions.js
@@ -0,0 +1,143 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * IN CLASS EXERCISE: FIRST CLASS FUNCTIONS
+ */
+
+/** 
+ * Given an input base to test against, which could be a String or Number, 
+ * return a Function that tests whether a given value is greater than the 
+ * base.
+ * test => true or false
+ */
+function createGreaterThanFilter(base) {
+    // YOUR CODE BELOW HERE //
+    // how to test if value is greaater than base?
+  return function(value){
+    if(value > base){
+        return true;
+    }else{
+        return false;
+    }
+
+
+  };
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input base to test against, which could be a String or Number, 
+ * return a Function that tests whether a given value is less than the 
+ * base.
+ */
+function createLessThanFilter(base) {
+    // YOUR CODE BELOW HERE //
+    
+    return function(value){
+        if(value < base){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given a startsWith character, which will be a single character, return a 
+ * Function that tests whether a given String starts with the startsWith 
+ * character.
+ */
+function createStartsWithFilter(startsWith) {
+    // YOUR CODE BELOW HERE //
+    
+    
+    
+   return function(inputString){
+        return inputString.startsWith(startsWith)
+   };
+    
+    
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given a endsWith character, which will be a single character, return a 
+ * Function that tests whether a given String ends with the endsWith 
+ * character.
+ */
+function createEndsWithFilter(endsWith) {
+    // YOUR CODE BELOW HERE //
+    
+    
+        return function(endsWithString){
+    return string.endsWith(endsWithString);
+        }
+    
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an Array of Strings and a Function designed to modify a String, 
+ * return the Array of the Strings, modified.
+ * 
+ * TIP: You need to loop over the Strings, right? We need to pass each String to 
+ * the modify Function, but we need to collect the results into some collection.
+ */
+function modifyStrings(strings, modify) {
+    // YOUR CODE BELOW HERE //
+   // create an empty Array. 
+    let modifiedStrings = [];
+// Loop over the Strings and push each String to modify Function. 
+for(let str of strings){
+    modifiedStrings.push(modify(str));
+}
+// return modifiedStrings
+    return modifiedStrings;
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an Array of Strings and a Function designed to test the String in some 
+ * way and return a Boolean on whether it passed, return true if ALL Strings pass the test.
+ * 
+ * Imagine you had a list of names, and you wanted to test they all 
+ * begin with "C", or they are all exclaimations that end with "!".
+ * 
+ * TIP: You need to loop over the Strings, right? And pass them to the test?
+ */
+function allStringsPass(strings, test) {// test is a function
+    // YOUR CODE BELOW HERE //
+  
+   
+    
+
+   
+
+    
+   
+    // YOUR CODE ABOVE HERE //
+}
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.createGreaterThanFilter = createGreaterThanFilter;
+    module.exports.createLessThanFilter = createLessThanFilter;
+    module.exports.createStartsWithFilter = createStartsWithFilter;
+    module.exports.createEndsWithFilter = createEndsWithFilter;
+    module.exports.modifyStrings = modifyStrings;
+    module.exports.allStringsPass = allStringsPass;   
+}
